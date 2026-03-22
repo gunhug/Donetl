74af1e41b7914a3fc76bf26bf319fb1cda1b91ab
Nguyễn Hùng <nqhung101203@gmail.com>
2026-03-23 02:24:48 +0700

    BA-001 - Refactor task controllers and model for improved functionality and consistency (#1)

--- backend/src/models/Task.js
+++ backend/src/models/Task.js
@@ -4,22 +4,22 @@ const taskSchema = new mongoose.Schema(
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     status: {
       type: String,
       enum: ["active", "completed"],
       default: "active",
     },
-    CompletedAt: {
+    completedAt: {
       type: Date,
       default: null,
     },
   },
   {
     timestamps: true, //tự động tạo 2 trường createdAt và updatedAt
-  }
+  },
 );
-
 
 const Task = mongoose.model("Task", taskSchema);
 export default Task;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,22 @@
-import express from 'express'
-import tasksRoutes from './routes/tasksRoutes.js'
-import {connectDB} from './config/db.js'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import tasksRoutes from "./routes/tasksRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-connectDB()
+const app = express();
 
-app.use('/api/tasks', tasksRoutes)
+connectDB();
 
-app.listen(PORT, () => {
-  console.log(`server bắt đầu trên cổng ${PORT}`)
-})
+app.use(express.json());
 
+app.use("/api/tasks", tasksRoutes);
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`server bắt đầu trên cổng ${PORT}`);
+  });
+});
