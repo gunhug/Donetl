10a9085137eaaefa936b3256974c34db9ed17efe
gunhug <nqhung101203@gmail.com>
2026-04-03 00:50:05 +0700

    FE-006 - Add UI components for task management and enhance layout - Implement AddTask component with input and button for adding tasks. - Update Header component with title and description. - Enhance StatsAndFilters component to display task statistics and filters. - Integrate components in HomePage for improved layout and functionality. - Introduce FilterType in data.js for task filtering options. - Add gradient button variant in button component.

---
+++ frontend/src/lib/data.js
@@ -0,0 +1,7 @@
+export const FilterType = {
+    all: "all",
+    completed: "completed",
+    pending: "pending",
+}
+
+// FilterType['All']
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,49 @@
 import React from "react";
+import Header from "@/components/Header";
+import AddTask from "@/components/AddTask";
+import StatsAndFilters from "@/components/StatsAndFilters";
+import TaskList from "@/components/TaskList";
+import TaskListPagination from "@/components/TaskListPagination";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from "@/components/Footer";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen w-full bg-white relative">
+      {/* Teal Glow Background */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #14b8a6 100%)
+      `,
+          backgroundSize: "100% 100%",
+        }}
+      />
+      <div className="container pt-8 mx-auto relative z-10">
+        <div className="w-full max-w-2xl p-6 mx-auto space-y-6">
+          {/* đầu trang */}
+          <Header />
+
+          {/* tạo nhiệm vụ */}
+          <AddTask />
+
+          {/* Thống kê và bộ lọc */}
+          <StatsAndFilters />
+
+          {/* Danh sách nhiệm vụ */}
+          <TaskList />
+
+          {/* Phân trang và lọc theo date */}
+          <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <TaskListPagination />
+            <DateTimeFilter />
+          </div>
+
+          {/* Chân trang */}
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
