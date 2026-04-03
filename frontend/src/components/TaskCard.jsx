62c7d10b0e67541989dfe207bdab006c3394b4d5
Nguyễn Hùng <nqhung101203@gmail.com>
2026-04-04 01:35:35 +0700

    FE-007 - Enhance task management UI with new components and improved functionality (#8)

---
+++ frontend/src/components/TaskCard.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
+import {
+  Calendar,
+  CheckCircle2,
+  Circle,
+  SquarePen,
+  Trash2,
+} from "lucide-react";
+import { Button } from "./ui/button";
+
+const TaskCard = ({ task, index }) => {
+  let isEditing = false;
+  return (
+    <Card
+      className={cn(
+        "p-4 border-0 bg-gradient-card shadow-custom-md hover:shadow-custom-lg transition-all duration-200 animate-fade-in group",
+        task.status === "completed" && "opacity-70",
+      )}
+      style={{ animationDelay: `${index * 50}ms` }}
+    >
+      <div className="flex items-center gap-4">
+        {/* Nút tròn */}
+        <Button
+          variant="ghost"
+          size="icon"
+          className={cn(
+            "flex-shrink-0 size-8 rounded-full transition-all duration-200",
+            task.status === "completed"
+              ? "text-success hover:text-success/80"
+              : "text-muted hover:text-primary",
+          )}
+        >
+          {task.status === "completed" ? (
+            <CheckCircle2 className="size-5" />
+          ) : (
+            <Circle className="size-5" />
+          )}
+        </Button>
+
+        {/* Hiển thị hoặc chỉnh sửa  tiêu đề */}
+        <div className="flex-1 min-w-0">
+          {isEditing ? (
+            <Input
+              placeholder="Edit task title"
+              className="flex-1 h-12 text-base border-border/50 focus:border-primary/50 focus:ring-primary/20"
+              type="text"
+            />
+          ) : (
+            <p
+              className={cn(
+                "text-base transition-all duration-200",
+                task.status === "completed"
+                  ? "line-through text-muted-foreground"
+                  : "text-foreground",
+              )}
+            >
+              {task.title}
+            </p>
+          )}
+
+          {/* Ngày tạo & ngày hoàn thành */}
+
+          <div className="flex items-center gap-2 mt-1">
+            <Calendar className="size-3 text-muted-foreground" />
+            <span className="text-xs text-muted-foreground">
+              {new Date(task.createdAt).toLocaleString()}
+            </span>
+            {task.completedAt && (
+              <>
+                <span className="text-xs text-muted-foreground"> - </span>
+                <Calendar className="size-3 text-muted-foreground" />
+                <span className="text-xs text-muted-foreground">
+                  {new Date(task.completedAt).toLocaleString()}
+                </span>
+              </>
+            )}
+          </div>
+        </div>
+
+        {/* nút chỉnh và xóa */}
+        <div className="hidden gap-2 group-hover:inline-flex animate-slide-up">
+          {/* nút edit */}
+          <Button
+            variant="ghost"
+            size="icon"
+            className="flex-shrink-0 transition-colors size-8 text-muted-foreground hover:text-info"
+          >
+            <SquarePen className="size-4" />
+          </Button>
+
+          {/* nút delete */}
+          <Button
+            variant="ghost"
+            size="icon"
+            className="flex-shrink-0 transition-colors size-8 text-muted-foreground hover:text-destructive"
+          >
+            <Trash2 className="size-4" />
+          </Button>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default TaskCard;
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import TaskEmptyState from "./TaskEmptyState";
+import TaskCard from "./TaskCard";
 
 const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
-}
+  let filter = "all";
+  const filteredTasks = [
+    {
+      _id: 1,
+      title: "Task 1",
+      status: "pending",
+      completedAt: null,
+      createdAt: new Date(),
+    },
 
-export default TaskList
+    {
+      _id: 2,
+      title: "Task 2",
+      status: "completed",
+      completedAt: new Date(),
+      createdAt: new Date(),
+    },
+  ];
+
+  if (!filteredTasks || filteredTasks.length === 0) {
+    return <TaskEmptyState filter={filter} />;
+  }
+  return (
+    <div className="space-y-3">
+      {filteredTasks.map((task, index) => (
+        <TaskCard key={task._id ?? index} task={task} index={index} />
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
