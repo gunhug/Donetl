b32dea32d10ae19addda3d8d382092615fc1d1a1
Nguyễn Hùng <nqhung101203@gmail.com>
2026-04-07 23:55:35 +0700

    DLT-012 - Add pagination component and integrate with task list (#14)

--- frontend/src/components/TaskListPagination.jsx
+++ frontend/src/components/TaskListPagination.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { cn } from "@/lib/utils";
 
-const TaskListPagination = () => {
+const TaskListPagination = ({
+  handleNext,
+  handlePrev,
+  handlePageChange,
+  currentPage,
+  totalPages,
+}) => {
+  const generatePages = () => {
+    const pages = [];
+
+    if (totalPages <= 4) {
+      // hiện toàn bộ
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push(i);
+      }
+    } else {
+      if (pages <= 2) {
+        pages.push(1, 2, 3, "...", totalPages);
+      } else if (currentPage >= totalPages - 1) {
+        pages.push(1, "...", totalPages - 2, totalPages - 1, totalPages);
+      } else {
+        pages.push(1, "...", currentPage, "...", totalPages);
+      }
+    }
+    return pages;
+  };
+
+  const pagesToShow = generatePages();
+
   return (
-    <div>TaskListPagination</div>
-  )
-}
+    <div className="flex justify-center mt-4">
+      <Pagination>
+        <PaginationContent>
+          {/* trang trước */}
+          <PaginationItem>
+            <PaginationPrevious
+              onClick={currentPage === 1 ? undefined : handlePrev}
+              className={cn(
+                "cursor-pointer",
+                currentPage === 1 && "pointer-events-none opacity-50",
+              )}
+            />
+          </PaginationItem>
 
-export default TaskListPagination
+          {pagesToShow.map((p, index) => (
+            <PaginationItem key={index}>
+              {p === "..." ? (
+                <PaginationEllipsis />
+              ) : (
+                <PaginationLink
+                  isActive={p === currentPage}
+                  onClick={() => {
+                    if (p !== currentPage) {
+                      handlePageChange(p);
+                    }
+                  }}
+                  className="cursor-pointer"
+                >
+                  {p}
+                </PaginationLink>
+              )}
+            </PaginationItem>
+          ))}
+
+          {/* trang tiếp theo */}
+          <PaginationItem>
+            <PaginationNext
+              onClick={currentPage === totalPages ? undefined : handleNext}
+              className={cn(
+                "cursor-pointer",
+                currentPage === totalPages && "pointer-events-none opacity-50",
+              )}
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+};
+
+export default TaskListPagination;
--- frontend/src/lib/data.js
+++ frontend/src/lib/data.js
@@ -24,3 +24,5 @@ export const options = [
     label: "All Tasks",
   },
 ];
+
+export const visibleTaskLimit = 4;
